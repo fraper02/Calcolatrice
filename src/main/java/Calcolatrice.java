f7f0306898e7210ca6e945809bbdef0cf507e71f
frankperilli <f.perilli2@studenti.unisa.it>
2023-09-25 19:35:59 +0100

    Upload calcolatrice

--- src/main/java/Calcolatrice.java
+++ src/main/java/Calcolatrice.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Calcolatrice {
+public class Calcolatrice {
+    private int a, b;
+
+    public Calcolatrice() {
+        this.a = 0;
+        this.b = 0;
+    }
+
+    public int somma(int a, int b){
+        return a+b;
+    }
+
+    public int moltiplicazione(int a,int b){
+        return a*b;
+    }
 }
